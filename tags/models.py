3a807b5b263074edb276a30140a459381e543263
Saman-naruee <samannaruee@gmail.com>
2024-08-29 17:01:46 +0330

    add likes app for liked items and update admin

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Generic relationships
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
 
-# Create your models here.
+class TaggedItem(models.Model):
+    # what tag applied to what object:
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # type(videos, images, articles, product, whatever ...)
+    # ID
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+    ...
