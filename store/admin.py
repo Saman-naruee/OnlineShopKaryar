67338f3ef81662f3cdbdced796782d9b89eb71a6
Saman-naruee <samannaruee@gmail.com>
2024-09-03 20:49:05 +0330

    update store/admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+admin.site.register(Customer)
+admin.site.register(Promotion)
+admin.site.register(Collection)
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+
