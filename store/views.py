14b3323bec530477041c0a7ae624fb68e2beb47b
Saman-naruee <samannaruee@gmail.com>
2024-09-08 01:22:28 +0330

    simple views for products and product details using rest_framework, set urls.

--- store/views.py
+++ store/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
 
-# Create your views here.
+
+@api_view()
+def product_list(request):
+    products = Product.objects.all()
+    data = {
+        'products': list(products.values('title', 'unit_price', 'inventory'))[:4]
+    }
+    return Response(data)
+
+@api_view(['GET'])
+def product_detail(request, id):
+    try:
+        product = Product.objects.get(pk=id)
+        data = {
+            'title': product.title,
+            'description': product.description
+        }
+        return Response(data, status=status.HTTP_200_OK)
+    except Product.DoesNotExist:
+        return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
