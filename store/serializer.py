de3bffc58931945ef4e96b139a201bb7f11e7c41
Saman-naruee <samannaruee@gmail.com>
2024-09-09 16:07:12 +0330

    update serializers as ModelSerializer

--- store/serializer.py
+++ store/serializer.py
@@ -2,19 +2,16 @@ from rest_framework import serializers
 from decimal import Decimal
 from .models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection'] # we can keep other non-existing fields down the bottom like before.
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField( # all way to serialize a relation: 1.primary key 2.string 3.Nested object 4.Hyperlinks
-        queryset = Collection.objects.all(),
-        view_name = 'collection-detail'
-    )
-
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.09)   # .quantize(Decimal('0.01'))
