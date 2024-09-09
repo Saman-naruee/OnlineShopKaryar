1becac6181e65287a58ff74b296cc7236b084ac4
Saman-naruee <samannaruee@gmail.com>
2024-09-09 15:33:01 +0330

    one more way to serialize a relationship: Create own class.

--- store/serializer.py
+++ store/serializer.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from decimal import Decimal
 from .models import Product, Collection
+
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length = 255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
-        return product.unit_price * Decimal(1.09)
+        return product.unit_price * Decimal(1.09)   # .quantize(Decimal('0.01'))
