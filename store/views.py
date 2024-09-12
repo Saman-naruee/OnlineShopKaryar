4d5f60260ec962a40530dea299ce31261077f626
Saman-naruee <samannaruee@gmail.com>
2024-09-12 18:48:52 +0330

    Using viewsets for combine multiple related views inside a single class: it is a set of related views.

--- store/views.py
+++ store/views.py
@@ -8,6 +8,39 @@ from rest_framework.views import APIView
 from .models import Product, Collection, OrderItem
 from .serializer import ProductSerializer, CollectionSerializer
 
+from rest_framework.viewsets import ModelViewSet
+
+class ProductViewset(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return {'request': self.request}
+
+    def delete(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        if OrderItem.objects.filter(product=product).exists():
+            return Response({'error': 'product can not be deleted, we have orderitems!'}, 
+                        status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
+
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection, pk=pk)
+        products_count = Product.objects.filter(collection=collection).count()
+        if products_count > 0:
+            return Response({'error':'can not delete because there is products associated with this collection'},
+                        status=status.HTTP_403_FORBIDDEN
+                        )
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+ # Using viewsets for combine multiple related viws inside a single class: it is a set of related views.
+
 class ProductList(ListCreateAPIView):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
