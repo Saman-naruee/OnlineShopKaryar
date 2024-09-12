876bbccd003df1582b3ff347300e5be7dbc38890
Saman-naruee <samannaruee@gmail.com>
2024-09-13 02:15:33 +0330

    sorting data: using drf filters.orderingfilter

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Product, Collection, OrderItem, Review
 from .serializer import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -13,9 +13,10 @@ from rest_framework.viewsets import ModelViewSet
 class ProductViewset(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
     search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
     
     def get_serializer_context(self):
         return {'request': self.request}
