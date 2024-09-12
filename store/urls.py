3a218b51f13b0c5c48d31650973420ae6b1f175a
Saman-naruee <samannaruee@gmail.com>
2024-09-12 19:23:19 +0330

    UPDATE: SET VIEWS AS VIEWSETS AND DELETE COMMITED CODES, SET URLS USING ROUTER.

--- store/urls.py
+++ store/urls.py
@@ -6,11 +6,3 @@ router = DefaultRouter()
 router.register('products', ProductViewset)
 router.register('collections', CollectionViewSet)
 urlpatterns = router.urls 
-
-
-# urlpatterns = [ 
-    # path('products/', ProductList.as_view()),
-    # path('products/<int:pk>/', ProductDetail.as_view()),
-    # path('collections/', CollectionList.as_view()),
-    # path('collections/<int:pk>/', CollectionDetail.as_view()),
-# ]
--- store/views.py
+++ store/views.py
@@ -23,14 +23,6 @@ class ProductViewset(ModelViewSet):
                         status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
 
-    # def delete(self, request, pk):
-    #     product = get_object_or_404(Product, pk=pk)
-    #     if OrderItem.objects.filter(product=product).exists():
-    #         return Response({'error': 'product can not be deleted, we have orderitems!'}, 
-    #                     status=status.HTTP_405_METHOD_NOT_ALLOWED)
-    #     product.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
@@ -44,50 +36,4 @@ class CollectionViewSet(ModelViewSet):
                         )
         return super().destroy(request, *args, **kwargs)
 
-    # def delete(self, request, pk):
-    #     collection = get_object_or_404(Collection, pk=pk)
-    #     products_count = Product.objects.filter(collection=collection).count()
-    #     if products_count > 0:
-    #         return Response({'error':'can not delete because there is products associated with this collection'},
-    #                     status=status.HTTP_403_FORBIDDEN
-    #                     )
-    #     collection.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
- # Using viewsets for combine multiple related viws inside a single class: it is a set of related views.
-
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        return {'request': self.request}
-
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    serializer_class = CollectionSerializer
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if OrderItem.objects.filter(product=product).exists():
-            return Response({'error': 'product can not be deleted, we have orderitems!'}, 
-                        status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.all()
-    serializer_class = CollectionSerializer
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        products_count = Product.objects.filter(collection=collection).count()
-        if products_count > 0:
-            return Response({'error':'can not delete because there is products associated with this collection'},
-                        status=status.HTTP_403_FORBIDDEN
-                        )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
