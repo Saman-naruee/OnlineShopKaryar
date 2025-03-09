f3f87d3b5b71eea582ac95200563cfeef7ae03e8
Saman-naruee <samannaruee@gmail.com>
2025-03-09 16:54:26 +0330

    Update authentications and authorization settings: just for products is readonly for everyone and for other endpoints it is authenticated.

--- storefront/urls.py
+++ storefront/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
         license=openapi.License(name='BSD License'),
     ),
     public=True,
-    permission_classes=(IsAuthenticated,)
+    permission_classes=[]  # ✅ No authentication required for Swagger UI, Actual values is: (IsAuthenticated,)
 )
 
 urlpatterns = [
