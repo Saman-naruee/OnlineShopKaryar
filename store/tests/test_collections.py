258559fc6fce063c10355b385633418b5ada1dc6
SamanNaruee <narueesaman@gmail.com>
2025-05-12 19:20:20 +0330

    update first pytest based tests logic.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -4,14 +4,14 @@ from rest_framework import status
 client = APIClient()
 
 
-def test_if_user_is_anonymous_then_returns_403(client):
+def test_if_user_is_anonymous_then_returns_401(client):
     # AAA (Arrange, Act, Assert)
     # Arrange
 
     # Act
-    respones = client.post("/store/collections/", {"name": "test"})
+    response = client.post("/store/collections/", {"name": "test"})
     # Assert
-    assert respones.status_code == status.HTTP_403_FORBIDDEN
-    assert respones.data["detail"] == "Authentication credentials were not provided."
-    
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.data["detail"] == "Authentication credentials were not provided."
+
 
