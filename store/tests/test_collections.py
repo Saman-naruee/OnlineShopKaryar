01d03a85551215d229c33e01eb1095863e0f1588
SamanNaruee <narueesaman@gmail.com>
2025-05-15 23:17:54 +0330

    update test as well logic.

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -4,14 +4,14 @@ import pytest
 client = APIClient()
 
 @pytest.mark.django_db
-def test_if_user_is_anonymous_then_returns_401():
+def test_if_user_is_anonymous_then_returns_403():
     # AAA (Arrange, Act, Assert)
     # Arrange
 
     # Act
     response = client.post("/store/collections/", {"title": "test"})
     # Assert
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.status_code == status.HTTP_403_FORBIDDEN
     assert response.data["detail"] == "Authentication credentials were not provided."
 
 
