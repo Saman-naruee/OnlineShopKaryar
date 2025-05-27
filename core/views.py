56d1ddea0aa6f172fac6a1fb5d04071fed935412
Saman-naruee <samannaruee@gmail.com>
2025-05-27 21:10:45 +0330

    Update core views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from typing import override
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from store import serializer
+from .serializers import UserSerializer, UserCreateSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    """
+    A view for registering new users.
+    """
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = UserCreateSerializer
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    """
+    A view for retrieving and updating user profiles.
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        if 'email' in request.data and request.data['email'] != instance.email:
+            return Response({'error': 'Email cannot be changed.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
