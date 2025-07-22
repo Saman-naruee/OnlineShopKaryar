2454871d99be53e2dbf2c4d6ee1275f0f427ddf2
Saman-naruee <samannaruee@gmail.com>
2025-07-22 22:33:34 +0330

    feat: Configure Redis as default cache backend
    Integrates django-redis to enable Redis as the default caching mechanism.
    This sets up the connection to a local Redis instance on edis://127.0.0.1:6379/2.

--- storefront/settings.py
+++ storefront/settings.py
@@ -255,3 +255,14 @@ CELERY_BEAT_SCHEDULE = {
         'args': ('Hello Celery!',)
     },
 }
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
