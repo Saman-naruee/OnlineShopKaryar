320559d023c5c36274716286ec3a4732aba92996
Saman-naruee <samannaruee@gmail.com>
2025-07-23 17:40:17 +0330

    update static html,css files to have better appearence. Stop tracking db.sqlite3 and add it to .gitignore

--- storefront/settings.py
+++ storefront/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
